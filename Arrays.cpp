b6bfc518e77fb6107195c26eedd2f7603348d8c0
deepakk14 <deepakk14.06.01@gmail.com>
2025-03-15 21:13:22 +0530

    Initial commit: Added arrays.cpp with basic array example

---
+++ Arrays.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare an array of 5 integers
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    // Print all elements
+    for (int i = 0; i < 5; i++) {
+        cout << "Element at index " << i << ": " << arr[i] << endl;
+    }
+
+    return 0;
+}
