2487bffde85a4f9a64f201111878d1ea058ade25
deepakk14 <deepakk14.06.01@gmail.com>
2025-03-23 11:14:39 +0530

    new Program

---
+++ Program_of_Array/Encode_Decode.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Codec {
+public:
+    // Encode: List of strings -> Single string
+    string encode(vector<string>& strs) {
+        string result = "";
+        for (const string& s : strs) {
+            result += to_string(s.length()) + "#" + s;
+        }
+        return result;
+    }
+    
+    // Decode: Single string -> List of strings
+    vector<string> decode(string s) {
+        vector<string> result;
+        int i = 0;
+        
+        while (i < s.length()) {
+            // Find the delimiter '#'
+            int j = i;
+            while (j < s.length() && s[j] != '#') {
+                j++;
+            }
+            
+            // Extract length (from i to j-1)
+            int length = stoi(s.substr(i, j - i));
+            
+            // Extract string (after '#' for 'length' characters)
+            string str = s.substr(j + 1, length);
+            result.push_back(str);
+            
+            // Move past this string
+            i = j + 1 + length;
+        }
+        
+        return result;
+    }
+};
+
+int main() {
+    Codec codec;
+    vector<string> strs = {"Hello", "World"};
+    
+    // Test encoding
+    string encoded = codec.encode(strs);
+    cout << "Encoded: " << encoded << endl;
+    
+    // Test decoding
+    vector<string> decoded = codec.decode(encoded);
+    cout << "Decoded: ";
+    for (const string& s : decoded) {
+        cout << s << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
